c207b841cb6792724224dadc202ffe2599933cd8
KingCreatBug <trunghoang.240500@gmail.com>
2024-09-03 17:16:57 +0700

    fix chat AI, add explore page

--- Styles_UI/assets/js/main.js
+++ Styles_UI/assets/js/main.js
@@ -115,45 +115,6 @@ function moveLeftBanner() {
     document.getElementById("bannerLogin").style.left = "0";
 }
 
-//Push Image
-document.getElementById("pushImage").onclick = function () {
-    let input = document.createElement("input");
-    input.type = "file";
-    input.onchange = (_) => {
-        let files = Array.from(input.files);
-        console.log(files);
-    };
-    input.click();
-};
-
-//Push microphone
-document.getElementById("mic").onclick = function () {
-    let output = document.getElementById("message");
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-    let recognization = new SpeechRecognition();
-    recognization.onstart = () => {};
-    recognization.onresult = (e) => {
-        var transcript = e.results[0][0].transcript;
-        output.innerText = transcript;
-    };
-    recognization.start();
-};
-
-//Chat message
-function message(text) {
-    return `<p
-    id="messageUser"
-    class="chat-message__text"
->${text}</p>`;
-}
-
-document.getElementById("send").onclick = function () {
-    let mess = document.getElementById("inputMessage").value;
-    document.getElementById("chat-box__wrap").style.display = "none";
-    document.getElementById("inputMessage").value = "";
-    document.getElementById("chat").innerHTML += message(mess);
-};
-
 //Carousel
 const carousel = document.querySelector(".feedback-container__carousel"),
     firstItem = document.querySelectorAll(".feedback-container__box")[0],
